58c95777de0ee98325e9a4a086feab04ede25670
[eschwelgin] <[eschwelgin@gmail.com]>
2020-03-05 15:47:03 -0500

    the real one

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,138 +1,152 @@
 var socket = io()
 const gameDiv = document.getElementById('game')
-let localUser = ''
+let localUser
+let userName
 let logged = false
+// let instrOn = true
+const instrBox = document.getElementById('instr')
 
-//---------------------------window size-----------------------
 let width, height
 function background(color) {
-    // if (pass === null) {color = pass} 
     if ( height > width && color == "blue" ) { 
         document.body.style.backgroundImage = "url(/assets/blue-vert.gif)"
-        // document.getElementsByTagName('button').style.borderColor = "#00BAFF" // im having trouble setting the button border colors dynamically 
     } else if (width > height && color == "blue") {
         document.body.style.backgroundImage = "url(/assets/blue-hori.gif)"
-        // document.getElementsByTagName('button').style.borderColor = "#00BAFF"
     } else if (height > width && color == "red") {
         document.body.style.backgroundImage = "url(/assets/red-vert.gif)"
-        // document.getElementsByTagName('button').style.borderColor = "#ff4500"
     } else if (width > height && color == "red") {
         document.body.style.backgroundImage = "url(/assets/red-hori.gif)"
-        // document.getElementsByTagName('button').style.borderColor = "#ff4500"
     } else {
         document.body.style.backgroundImage = "url(/assets/blue-static.jpg)"
-        // document.getElementsByTagName('button').style.borderColor = "#00BAFF"
     }
-// console.log(`width = ${width} and height = ${height}`)
 }
 
-window.addEventListener ("load", function() { // old school event listeners, same as what were doing with socket, except different, but not really 
+function onLoad(user) {
     width = window.innerWidth
     height = window.innerHeight
-})
+    localUser = user
+    logged = true
+    socket.emit('username', localUser) 
+    socket.emit('game-start')
+}
+window.addEventListener ("load", onLoad() )
 
 window.addEventListener ("resize", function() {
     width = window.innerWidth
     height = window.innerHeight
 })
-//----------------------end window size----------------------
 
-function sendPress(id) { // we're calling these functions using onClick() in html. When we create the buttons later in the page, we're going to dynamically assign them a function 
+function inst() {
+    save = gameDiv.innerHTML
+    gameDiv.innerHTML = `<h3>Enter your name and press Start Game</h3>
+                        <h3>Scavenger Hunt! Spot any of these things in the crowd, and be the first to press the button</h3>
+                        <h3>The first player to press the button gets to choose an upcoming dare</h3>
+                        <h3>The second person to spot the item and press the button is exempt from the coming dare</h3>
+                        <h3>Choose a dare, choose a target, and make your buddys embarass themselves!</h3>
+                        <h3>Blue screens are present when the game is in waiting mode, keep looking for things in the crowd!</h3>
+                        <h3>Red screens mean danger. The screen will flash red when first place has found something, but second place has not registered yet. </h3>
+                        <h3>It will also flash red during the dare stage, watch out!</h3>`
+    setTimeout(function(){gameDiv.innerHTML = save}, 10000)
+}
+
+function listUsers(){
+    gameDiv.innerHTML = ''
+    for (i=0; i<userName.length; i++) {
+        const button = document.createElement('button') // button 
+        button.setAttribute('onClick', `onLoad("${userName[i]}")`)
+        button.style.borderColor = "#00BAFF"
+        button.appendChild(document.createTextNode(userName[i]))
+        gameDiv.appendChild(button) 
+    }
+}
+
+function sendPress(id) {
     socket.emit('button-press', JSON.stringify({
         "id": id,
         "localUser": localUser, 
-        "socketId": socket.id
     }))
 }
 
-function secondPress(id) { // in the server the first click and the second click are different (first palce and second palce). they run different functiuons when clicked. 
+function secondPress(id) {
     socket.emit('second-press', JSON.stringify({
         "id": id,
         "localUser": localUser
     }))
 }
 
-function setUser() { // this is the function the button calls on the main menu
-    localUser = document.getElementById('playerName').value
-    logged = true
-    socket.emit('username', localUser) // emit event to set username, sends the event along with username data to the server 
-    socket.emit('game-start') // new event that tells the server at least one person is in the game, the serverin return fires the load-buttons event with (scavenger) data 
+function thirdPress() {
+    socket.emit('game-start')
 }
 
-socket.on('logo-screen', function(){ // socket.on listens for an event, and fires when it gets it. we can emit and listen on both the server and client. 
- 
-    gameDiv.innerHTML=''
-
-        const picture = document.createElement('img') //create logo picture 
-        picture.setAttribute('src', './assets/70581301-infinite-game-design-template.jpg')
-        picture.setAttribute('class', 'logo')
-        gameDiv.appendChild(picture)
-
-        const br0 = document.createElement('br') // line break
-        gameDiv.appendChild(br0)
-
-        const nameBox = document.createElement('input') // input box for player name 
-        nameBox.setAttribute('id', "playerName")
-        gameDiv.appendChild(nameBox)
-
-        const br1 = document.createElement('br') // line break 
-        gameDiv.appendChild(br1)
-
-        const button = document.createElement('button') // button 
-        button.setAttribute('onClick', "setUser()")
-        button.appendChild(document.createTextNode("START GAME!"))
-        button.style.borderColor = "#00BAFF" // this is that line that was fucking me up 
-        gameDiv.appendChild(button)
-
-        background("blue")   
-    
-})
-
-socket.on('load-buttons', function(buttonData){ // a listener and a function for when we want to load buttons. we can load whatever we want on the buttons depending on what date we send. we use this funtion for both scavenger data and dare data 
-    console.log(buttonData)
-  if(logged) {
-    gameDiv.innerHTML=''
-    for (i = 0; i < buttonData.length; i++) {
-        const br = document.createElement('br') // dynamically creating dom elements just like we did before 
-        gameDiv.appendChild(br)
-        const newBtn = document.createElement("button")
-        newBtn.setAttribute('id', i)
-        newBtn.setAttribute('onClick', 'sendPress(this.id)')
-        newBtn.style.borderColor = "#00BAFF"
-        newBtn.appendChild(document.createTextNode(buttonData[i])) 
-        gameDiv.appendChild(newBtn)
-    }
+function setUser() { 
+        localUser = document.getElementById('playerName').value
+        console.log(localUser)
+        logged = true
+        socket.emit('username', localUser) 
+        socket.emit('game-start') 
 }
-})
 
-socket.on('load-buttons2', function(buttonData){ // the only difference here is the function the buttons call when you click them 
-    if(logged) {
+socket.on('logo-screen', function(userName0){
+    logged = false
     gameDiv.innerHTML=''
-    for (i = 0; i < buttonData.length; i++) {
-        const br = document.createElement('br')
-        gameDiv.appendChild(br)
-        const newBtn = document.createElement("button")
-        newBtn.setAttribute('id', i)
-        newBtn.setAttribute('onClick', 'secondPress(this.id)')
-        newBtn.style.borderColor = "#ff4500"
-        newBtn.appendChild(document.createTextNode(buttonData[i])) 
-        gameDiv.appendChild(newBtn)
-    }
-    background("red")
-    }
+    userName = userName0
+    const picture = document.createElement('img') //create logo picture up top 
+    picture.setAttribute('src', './assets/logo.png')
+    picture.setAttribute('class', 'logo')
+    gameDiv.appendChild(picture)
+
+    const br0 = document.createElement('br') // line break
+    gameDiv.appendChild(br0)
+
+    const nameBox = document.createElement('input') // input box for player name 
+    nameBox.setAttribute('id', "playerName")
+    gameDiv.appendChild(nameBox)
+
+    const br1 = document.createElement('br') // line break 
+    gameDiv.appendChild(br1)
+
+    const button = document.createElement('button') // button 
+    button.setAttribute('onClick', "setUser()")
+    button.style.borderColor = "#00BAFF"
+    button.appendChild(document.createTextNode("START GAME!"))
+    gameDiv.appendChild(button)   
+
+    const button0 = document.createElement('button') // button 
+    button0.setAttribute('onClick', "listUsers()")
+    button0.style.borderColor = "#00BAFF"
+    button0.appendChild(document.createTextNode("Already Logged In? Click Here"))
+    gameDiv.appendChild(button0)
+
+    background("blue")
 })
 
-socket.on('load-list', function(buttonData){ // pretty much the same as load-buttons 1 and 2, except we're loading the data into p tags instead of buttons, so that they're not clickable 
+// socket.on('instructions', function(data) {
+//     if (instrOn) {
+//         if (logged) {
+//             gameDiv.innerHTML=''
+//             const para = document.createElement('p')
+//             para.appendChild(document.createTextNode(data))
+//             gameDiv.appendChild(para)
+//         }
+//     }
+// })
+
+socket.on('load-buttons', function([buttonData, bl, fn, color]) { //data, button or p, what function is called, color of background
     if(logged) {
-    gameDiv.innerHTML=''
-    for (i = 0; i < buttonData.length; i++) {
-        const br = document.createElement('br')
-        gameDiv.appendChild(br)
-        const para = document.createElement("p")
-        para.appendChild(document.createTextNode(buttonData[i])) 
-        gameDiv.appendChild(para)
+        gameDiv.innerHTML=''
+        for (i = 0; i < buttonData.length; i++) {
+            const br = document.createElement('br')
+            gameDiv.appendChild(br)
+            const ele = document.createElement(bl)
+            if (bl === "button") { 
+                ele.setAttribute('id', i)
+                ele.setAttribute('onClick', `${fn}(this.id)`)
+                if (color == "blue") { ele.style.borderColor = "#00BAFF"}
+                if (color == "red") { ele.style.borderColor = "#ff4500"}
+            }
+            ele.appendChild(document.createTextNode(buttonData[i])) 
+            gameDiv.appendChild(ele)
+        }
     }
-    background("red")
-    }
-})
-
+    background(color)
+}) 
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const path = require('path');
 const express = require('express');
 const mysql = require('mysql')
-
 const app = express();
-const http = require('http').createServer(app); // this is the block that we're working with with socket.io - express, http, and socket are all required 
+const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
 const orm = require('./orm/orm.js')
 const PORT = process.env.PORT || 3000;
 
 let firstPlace
 let secondPlace
+let user = []
 
 let objScav
 orm.scav().then(values => { 
@@ -21,76 +20,61 @@ let objDare
 Promise.all([orm.returnOne(), orm.returnOne(), orm.returnOne(), orm.returnOne(), orm.returnOne(), orm.returnOne()])
   .then(values => objDare = values)
 
-const user = []
-
-const fakeObjScav = [
-  {"sentence": "The stupud red hat example we (I) keep using"},
-  {"sentence": "A confused man"},
-  {"sentence": "A man hitting on an un-interested woman"},
-  {"sentence": "Someone spilled a drink"},
-  {"sentence": "Confused guy"},
-  {"sentence": "Someone on a date, but they're texing"}
-]
-
-const fakeObjDare = [
-  {"sentence": "Try and sell your shoes to the bartender"},
-  {"sentence": "Ask an angry man about your thumbs"},
-  {"sentence": "Offer your shoes to the bartender"},
-  {"sentence": "Get someone to buy you a drink"},
-  {"sentence": "Ask a woman about your eyebrows"},
-  {"sentence": "Try and sell your eyebrows to an angry man"}
-]
-
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
-io.on('connection', function(socket) { // io.on is our global listener, and assignes a socket to each user 
-  console.log('A user connected: ' + socket.id); // socket.id holds that value 
-  socket.emit('logo-screen'); // .emit is how we create the event "logo-screen" 
-  socket.on('username', function(data) { // .on is how we create a listener for the events we create 
-    
+io.on('connection', function(socket) {
+    console.log('A user connected: ' + socket.id);
+    socket.emit('logo-screen', user.map(user => user.userName));
+    socket.on('username', function(data) {
+    user.push({
+      "userName": data, // need to make sure none uses the same name upon login. the same search of var user can identify a user as being the same player if they get booted and have to log in again 
+      "userId": socket.id // upon login .find() to search for existing username. prompt player "is this you" yes/no if yes, overwrite socket.id, if no alert "username in use please pick different name"
+    })
     console.log(user)
   });
 
-   
-  socket.on('game-start', function() { // here we have a listener for "game-start" 
-      socket.emit('load-buttons', objScav) // and in response we emit another event "load-buttons" with scavenger data 
+  socket.on('game-start', function() {
+    // data = 'Scavenger Hunt! Spot any of these things in the crowd, and be the first to press the button'
+    // io.to(`${socket.id}`).emit('instructions', data)
+    io.emit('load-buttons', [objScav, 'button', 'sendPress', 'blue'])
+    Promise.all([orm.returnOne(), orm.returnOne(), orm.returnOne(), orm.returnOne(), orm.returnOne(), orm.returnOne()])
+      .then(values => objDare = values)
   });
 
-  socket.on('button-press', function(data) { // listening for a button press
+  socket.on('button-press', function(data) {
     const parsed = JSON.parse(data)
-     firstPlace = user.find(user => user.userName === parsed.localUser) // do stuff 
+    firstPlace = user.find(user => user.userName === parsed.localUser)
     console.log('first place = ', firstPlace.userId)
-      socket.broadcast.emit('load-buttons2', objScav)
+    socket.broadcast.emit('load-buttons',  [objScav, 'button', 'secondPress', 'red'])
   });
 
-    socket.on('second-press', function(data) { // we create a second listener for the second press, so that we can do different "do stuff" 
-      // console.log('Second Place: ' + data)
-      const parsed = JSON.parse(data)
-       secondPlace = user.find(user => user.userName === parsed.localUser)
-      console.log('second place = ', secondPlace.userId)
-      // add logging - second place -> value
-      // socket.emit('load-buttons', objDare)
-      // socket.broadcast.emit('load-list', objDare)
-      // update()
-      
-      io.to(`${firstPlace.userId}`).emit('load-buttons', objDare) // io.to allows us to emit to a speciufic user, using their socket.id 
-      const losers = user.filter(user => user.userId !== firstPlace.userId)
+  socket.on('second-press', function(data) {
+    orm.scav().then(values => {
+      objScav = values.map(({ scavenger_sentence }) => scavenger_sentence)
+    })
+    const parsed = JSON.parse(data)
+    secondPlace = user.find(user => user.userName === parsed.localUser)
+    console.log('second place = ', secondPlace.userId)
 
-      for (i=0; i<losers.length; i++) {
-          io.to(`${losers[i].userId}`).emit('load-list', objDare)
-      }
-        // console.log(objDare)
-
+    io.to(`${firstPlace.userId}`).emit('load-buttons', [objDare, 'button', 'thirdPress', 'red']);
+    io.to(`${secondPlace.userId}`).emit('load-buttons', [objDare, 'p', 'thirdPress', 'blue']) 
+    let losers = user.filter(user => user.userId !== firstPlace.userId)
+    let loosers = losers.filter(losers => losers.userId !== secondPlace.userId) 
+    console.log(loosers)
+    for (i=0; i<loosers.length; i++) {
+      io.to(`${loosers[i].userId}`).emit('load-buttons', [objDare, 'p', 'thirdPress', 'red']);
+    }
   });
 })
+app.post('/:table/:value', function(req, res) {
+  orm.add(req.params.table, req.params.value)
+  res.status(200)
+})
 
-
-// post endpoint (add new nouns / objects to tables?)
-
-http.listen(PORT, function () { // standard express listener, except we user http. instead of app. (idk why) 
+http.listen(PORT, function () {
   console.log(`Server running, listening on ${PORT}`)
 })
