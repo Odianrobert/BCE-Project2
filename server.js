f5fa6876c7dcf868d165f29ccd6cb737717a6e48
Bobby Odian <odianrobert@yahoo.com>
2020-03-05 07:22:01 -0500

    game will loop

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const io = require('socket.io')(http);
 const orm = require('./orm/orm.js')
 const PORT = process.env.PORT || 3000;
 
+
+
 let objScav
 orm.scav().then(values => {
   objScav = values.map(({ scavenger_sentence }) => scavenger_sentence)
@@ -18,6 +20,8 @@ Promise.all([orm.returnOne(), orm.returnOne(), orm.returnOne(), orm.returnOne(),
   .then(values => objDare = values)
 
 const user = []
+let firstPlace
+let secondPlace
 
 app.use(express.static('public'));
 
@@ -27,8 +31,7 @@ app.get('/', function (req, res) {
 
 io.on('connection', function(socket) {
   console.log('A user connected: ' + socket.id);
-  let firstPlace
-  let secondPlace
+  
   socket.emit('logo-screen');
   socket.on('username', function(data) {
     // console.log('Username: ' + data);
@@ -41,34 +44,38 @@ io.on('connection', function(socket) {
 
   
   socket.on('game-start', function() {
-    socket.emit('load-buttons', objScav)
+    Promise.all([orm.returnOne(), orm.returnOne(), orm.returnOne(), orm.returnOne(), orm.returnOne(), orm.returnOne()])
+  .then(values => objDare = values)
+    io.emit('load-buttons', objScav)
 });
+
 
   socket.on('button-press', function(data) {
     const parsed = JSON.parse(data)
     firstPlace = user.find(user => user.userName === parsed.localUser)
     console.log('first place = ', firstPlace.userId)
-    orm.scav().then(values => {
-      socket.broadcast.emit('load-buttons2',  values.map(({ scavenger_sentence }) => scavenger_sentence))
-    })
+      socket.broadcast.emit('load-buttons2',  objScav)
   });
 
     socket.on('second-press', function(data) {
+      orm.scav().then(values => {
+        objScav = values.map(({ scavenger_sentence }) => scavenger_sentence)
+      })
       // console.log('Second Place: ' + data)
       const parsed = JSON.parse(data)
        secondPlace = user.find(user => user.userName === parsed.localUser)
       console.log('second place = ', secondPlace.userId)
       
-      io.to(`${firstPlace.userId}`).emit('load-buttons', objDare);
+      io.to(`${firstPlace.userId}`).emit('load-buttons3', objDare);
       const losers = user.filter(user => user.userId !== firstPlace.userId)
 
       for (i=0; i<losers.length; i++) {
         io.to(`${losers[i].userId}`).emit('load-list', objDare);
       }
-        // console.log(objDare)
-
   });
 })
+
+
 
 
 // post endpoint (add new nouns / objects to tables?)
